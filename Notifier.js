d0dbc90743ba14aeab90095bbe30ab3533df4c4e
Ionut Moraru <morion4000@gmail.com>
2015-10-23 16:25:19 +0300

    Changed the Notifier to init the exchange outside of the notify method

--- Notifier.js
+++ Notifier.js
@@ -3,21 +3,20 @@ var errors = require('./errors');
 var Notifier = function(environment, exchangeName, amqpConnection) {
   var conn = amqpConnection;
   var env = environment;
+  var exchange;
+
+  conn.on('ready', function() {
+    console.log("rabbit ready");
+    exchange = conn.exchange(env + '.' + exchangeName, {
+      type: 'topic',
+      durable: true,
+      autoDelete: false
+    }, function(exchange) {
+      console.log('Exchange ' + exchange.name + ' is open');
+    });
+  });
 
   var notify = function(routing_key) {
-    var exchange;
-
-    conn.on('ready', function() {
-      console.log("rabbit ready");
-      exchange = conn.exchange(env + '.' + exchangeName, {
-        type: 'topic',
-        durable: true,
-        autoDelete: false
-      }, function(exchange) {
-        console.log('Exchange ' + exchange.name + ' is open');
-      });
-    });
-
     return function(req, res, next) {
       exchange.publish(env + '.' + routing_key, res.data);
       next();
