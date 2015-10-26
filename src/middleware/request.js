f935d16ab9d1267c92cd8cc12d55cdb00782f48c
Ionut Moraru <morion4000@gmail.com>
2015-10-26 16:53:01 +0200

    Added a body cleanup method for the Request middleware

--- src/middleware/request.js
+++ src/middleware/request.js
@@ -9,8 +9,37 @@ var Request = function() {
     }
   };
 
+  var body_cleanup = function(req, res, next) {
+    function cleanup(items) {
+      _.each(items, function(value, key) {
+        if (_.isObject(value) || _.isArray(value)) {
+          cleanup(value);
+        } else {
+          switch (value) {
+            case 'null':
+              items[key] = null;
+              break;
+
+            case 'true':
+              items[key] = true;
+              break;
+
+            case 'false':
+              items[key] = false;
+              break;
+          }
+        }
+      });
+    }
+
+    cleanup(req.body);
+
+    next();
+  };
+
   return {
-    end: end
+    end: end,
+    body_cleanup: body_cleanup
   };
 };
 
