7e3206f526d3af55398a094ae63585d577aee026
Ionut Moraru <morion4000@gmail.com>
2015-10-26 16:56:10 +0200

    Added middleware to check for valid fields

--- src/middleware/request.js
+++ src/middleware/request.js
@@ -1,4 +1,5 @@
-var _ = require('underscore');
+var _ = require('underscore'),
+  errors = require('./../errors');
 
 var Request = function() {
   var end = function(req, res, next) {
@@ -37,9 +38,50 @@ var Request = function() {
     next();
   };
 
+  var validate_fields = function(fields_to_validate) {
+    // TODO: Return error if fields are sent but are not in the validation list?
+
+    return function(req, res, next) {
+      var invalid_fields = [],
+        fields = req.body;
+
+      _.each(fields_to_validate, function(field_to_validate_value, field_to_validate_key) {
+        var found = false,
+          valid = false,
+          field_to_validate_value_defaulted;
+
+        field_to_validate_value_defaulted = _.defaults(field_to_validate_value, {
+          mandatory: false,
+          type: 'string'
+        });
+
+        _.each(fields, function(field_value, field_key) {
+          if (field_key === field_to_validate_key) {
+            found = true;
+
+            if (typeof field_value == field_to_validate_value_defaulted.type) {
+              valid = true;
+            }
+          }
+        });
+
+        if (!valid || (field_to_validate_value_defaulted.mandatory && !found)) {
+          invalid_fields.push(field_to_validate_key);
+        }
+      });
+
+      if (invalid_fields.length > 0) {
+        return next(new errors.validation_error('The following fields are invalid/required: ' + invalid_fields.join()));
+      }
+
+      next();
+    };
+  };
+
   return {
     end: end,
-    body_cleanup: body_cleanup
+    body_cleanup: body_cleanup,
+    validate_fields: validate_fields
   };
 };
 
