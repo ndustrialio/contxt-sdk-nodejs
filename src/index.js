5ba6a2b6668a9ebadbea0b19e4bc6db68ed811aa
Ionut Moraru <morion4000@gmail.com>
2015-10-27 14:36:09 +0200

    The sdk has to be initialized with parameters

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports.errors = require('./errors');
-module.exports.middleware = require('./middleware');
-module.exports.Notifier = require('./notifier');
+var _ = require('underscore'),
+  config = require('./config'),
+  errors = require('./errors'),
+  middleware = require('./middleware'),
+  Notifier = require('./notifier');
+
+var Sdk = function(options) {
+  var _options = _.defaults(options, {
+    environment: 'development',
+  });
+
+  config.set(_options);
+
+  return {
+    errors: errors,
+    middleware: middleware,
+    Notifier: Notifier
+  };
+};
+
+module.exports = Sdk;
