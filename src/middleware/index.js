58fa7845c2695e5971b1d6e4c971797268d4663a
Ionut Moraru <morion4000@gmail.com>
2015-10-28 17:09:41 +0200

    Fixed a small bug

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,4 @@
 module.exports.cors = require('./cors');
 module.exports.errors = require('./errors');
-module.exports.request = require('./request');
+module.exports.Request = require('./request');
 module.exports.Auth = require('./auth');
