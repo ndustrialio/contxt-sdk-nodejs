2a87af8fb176b5a4d2c6486bc0275fe10304aef9
Ionut Moraru <morion4000@gmail.com>
2015-10-29 11:05:07 +0200

    Fixed a bug with the Listener queue object being called before it was created

--- src/listener.js
+++ src/listener.js
@@ -11,23 +11,25 @@ var Listener = function(queue_name) {
       durable: true,
       autoDelete: false
     }, function(queue) {
-      //console.log('Queue ' + queue.name + ' is open');
+      console.log('Queue ' + queue.name + ' is created');
     });
   });
 
   var listen = function(exchange_name, routing_key, callback) {
-    queue.bind(environment + '.' + exchange_name, environment + '.' + routing_key);
+    rabbitmq_connection.on('ready', function() {
+      queue.bind(environment + '.' + exchange_name, environment + '.' + routing_key);
 
-    queue.subscribe(function(message, headers, delivery_info, message_object) {
-      var data = JSON.parse(message.data.toString()),
-        short_routing_key = delivery_info.routingKey;
+      queue.subscribe(function(message, headers, delivery_info, message_object) {
+        var data = JSON.parse(message.data.toString()),
+          short_routing_key = delivery_info.routingKey;
 
-      // Return the routing key without the environment for simplicity
-      short_routing_key = short_routing_key.split('.');
-      short_routing_key.shift();
-      short_routing_key = short_routing_key.join('.');
+        // Return the routing key without the environment for simplicity
+        short_routing_key = short_routing_key.split('.');
+        short_routing_key.shift();
+        short_routing_key = short_routing_key.join('.');
 
-      callback(short_routing_key, data);
+        callback(short_routing_key, data);
+      });
     });
   };
 
