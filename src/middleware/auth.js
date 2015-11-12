08dab20733c27096ab0dfee8c7acfdd8031988c5
Ionut Moraru <morion4000@gmail.com>
2015-11-12 18:20:59 +0200

    Changed token to be a string instead of an array for the Auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,7 +11,7 @@ var Auth = function(options) {
       return next(new errors.not_authorised('Access token not provided'));
     }
 
-    token = bearer.match(/[a-z0-9]{40}/g);
+    token = _.first(bearer.match(/[a-z0-9]{40}/g));
 
     if (!token) {
       return next(new errors.validation_error('Access token is malformed'));
