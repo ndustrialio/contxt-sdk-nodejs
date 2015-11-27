29002c936bba6d53df395163b09efd483ba0132d
Ionut Moraru <morion4000@gmail.com>
2015-11-27 18:55:57 +0200

    Refactored the Listener object
    Queues are now created from the parameters and parameters are sent as
    object literal

--- src/listener.js
+++ src/listener.js
@@ -5,53 +5,69 @@ var async = require('async'),
 var Listener = function(options) {
   var _config = config.get(),
     environment = _config.environment,
-    rabbitmq_connection = _config.rabbitmq_connection,
-    queue;
+    connection = _config.rabbitmq_connection,
+    queues = [];
 
-  rabbitmq_connection.on('ready', function() {
-    queue = rabbitmq_connection.queue(environment + '.' + options.queue_name, {
-      durable: true,
-      autoDelete: false
-    }, function(queue) {
-      console.log('Queue ' + queue.name + ' is created');
-    });
-  });
-
-  var listen = function(exchange_name, routing_key, callback) {
+  var _ready = function(callback) {
     async.retry({
       times: 10,
       interval: 1000
     }, function(callback) {
-      if (!_.isUndefined(queue)) {
+      if (connection.ready === true) {
         callback();
       } else {
-        callback('RabbitMQ queue is not ready.');
+        callback('RabbitMQ connection is taking too long to initialize.');
       }
-    }, function(err) {
+    }, callback);
+  };
+
+  var _create_binding = function(queue, options, callback) {
+    console.log('Binded queue', queue.name, '(', environment + '.' +
+      options.routing_key, ') to exchange', environment + '.' +
+      options.exchange_name);
+
+    queue.bind(environment + '.' + options.exchange_name,
+      environment + '.' + options.routing_key);
+
+    queue.subscribe(function(message, headers, delivery_info, message_object) {
+      console.log('Received message', message, 'on queue', queue.name,
+        'with routing key', delivery_info.routingKey);
+
+      var short_routing_key = delivery_info.routingKey;
+
+      // Return the routing key without the environment for simplicity
+      short_routing_key = short_routing_key.split('.');
+      short_routing_key.shift();
+      short_routing_key = short_routing_key.join('.');
+
+      callback(short_routing_key, message);
+    });
+  };
+
+  var listen = function(options, callback) {
+    _ready(function(err) {
       if (err) {
         console.log(err);
 
         return;
       }
 
-      console.log('Binded queue', queue.name, '(', environment + '.' +
-        routing_key, ') to queue', environment + '.' + exchange_name);
+      var queue;
 
-      queue.bind(environment + '.' + exchange_name, environment + '.' + routing_key);
+      if (_.contains(queues, options.queue_name)) {
+        queue = queues[options.queue_name];
 
-      queue.subscribe(function(message, headers, delivery_info, message_object) {
-        console.log('Received message', message, 'on queue', queue.name,
-          'with routing key', delivery_info.routingKey);
+        _create_binding(queue, options, callback);
+      } else {
+        queue = connection.queue(environment + '.' + options.queue_name, {
+          durable: true,
+          autoDelete: false
+        }, function(queue) {
+          console.log('Queue ' + queue.name + ' is created');
 
-        var short_routing_key = delivery_info.routingKey;
-
-        // Return the routing key without the environment for simplicity
-        short_routing_key = short_routing_key.split('.');
-        short_routing_key.shift();
-        short_routing_key = short_routing_key.join('.');
-
-        callback(short_routing_key, message);
-      });
+          _create_binding(queue, options, callback);
+        });
+      }
     });
   };
 
