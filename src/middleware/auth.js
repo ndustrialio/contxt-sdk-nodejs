23734f0bd0a5403f373980bd698b439015e09a49
Ionut Moraru <morion4000@gmail.com>
2016-04-14 13:14:49 +0300

    Implemented auth caching
    * Added new packages to package.json
    * Added a new option to the SDK component
    * Added a new options to the SDK.Auth component
    * Amended the SDK.Auth component to query redis for the auth cache

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,7 @@
 var request = require('request'),
   _ = require('underscore'),
+  Redis = require('redis'),
+  config = require('./../config'),
   errors = require('./../errors');
 
 /**
@@ -7,16 +9,82 @@ var request = require('request'),
  * @constructor
  * @param {object} options - The options.
  * @param {object} options.service_url - The OAuth2 service URL.
+ * @param {string} options.TOKEN_CACHE_EXPIRES - The expiration time of the auth token in seconds.
  * @static
  * @module middleware
  * @submodule auth
  * @main middleware
  * @namespace contxt-sdk-nodejs.sdk.middleware
  * @example
- *  var auth = require('contxt-sdk-nodejs').Sdk().middleware.Auth({service_url: 'test'});
+ *  var auth = require('contxt-sdk-nodejs').Sdk().middleware.Auth({
+ *    service_url: 'test'
+ *    TOKEN_CACHE_EXPIRES: 3600
+ *  });
  */
 
 var Auth = function(options) {
+  /**
+   * @property _options
+   * @private
+   * @type object
+   */
+  var _options = _.defaults(options, {
+    TOKEN_CACHE_EXPIRES: 3600 // 1h
+  });
+
+  /**
+   * @property _config
+   * @private
+   * @type object
+   */
+  var _config = config.get();
+
+  /**
+   * @property _redis_connection
+   * @private
+   * @type object
+   */
+  var _redis_connection = _config.redis_connection;
+
+  /**
+   * Call the auth service and return the parsed results.
+   *
+   * @member _call_auth_service
+   * @async
+   * @private
+   * @param {string} bearer - OAuth2 token bearer string.
+   * @param {function} callback - Callback to execute when the response is parsed.
+   */
+  var _call_auth_service = function(bearer, callback) {
+    request({
+      url: _options.service_url + '/users/current',
+      headers: {
+        'Authorization': bearer
+      }
+    }, function(err, response, body) {
+
+      if (err) {
+        return callback(new errors.interservice_error('Unable to authorise request. Authentication service unavailable'));
+      }
+
+      if (response.statusCode != 200) {
+        if (response.statusCode == 502) {
+          return callback(new errors.interservice_error('Unable to authorise request. Authentication service unavailable'));
+        } else {
+          return callback(new errors.not_authorised(body));
+        }
+      }
+
+      var result = JSON.parse(body);
+
+      // TODO: Better check for if the response body is an user object or an auth error
+      if (_.has(result, 'code')) {
+        return callback(result);
+      }
+
+      callback(null, result);
+    });
+  };
 
   /**
    * Call an OAuth endpoint to authorize the request.
@@ -44,36 +112,48 @@ var Auth = function(options) {
     // Store the token on the request object to be used further along the line
     req.token = token;
 
-    request({
-      url: options.service_url + '/users/current',
-      headers: {
-        'Authorization': bearer
-      }
-    }, function(error, response, body) {
+    if (_redis_connection) {
+      console.info('Found cache configuration. Trying to get token from cache...');
 
-      if (error) {
-        return next(new errors.interservice_error('Unable to authorise request. Authentication service unavailable'));
-      }
+      _redis_connection.hgetall(token, function(err, cached_user) {
+        if (cached_user === null || err) {
+          console.info('Token not found in cache. Trying service...');
 
-      if (response.statusCode != 200) {
-        if (response.statusCode == 502) {
-          return next(new errors.interservice_error('Unable to authorise request. Authentication service unavailable'));
+          _call_auth_service(bearer, function(err, result) {
+            if (err) {
+              return next(err);
+            }
+
+            // Set the token information in cache with expiration
+            _redis_connection.hmset(token, result, function() {
+              _redis_connection.expire(token, _options.TOKEN_CACHE_EXPIRES, Redis.print);
+            });
+
+            req.user = result;
+
+            next();
+          });
         } else {
-          return next(new errors.not_authorised(body));
+          console.info('Token found in cache.');
+
+          req.user = cached_user;
+
+          next();
         }
-      }
+      });
+    } else {
+      console.info('No cache configuration found. Trying service...');
 
-      var result = JSON.parse(body);
+      _call_auth_service(bearer, function(err, result) {
+        if (err) {
+          return next(err);
+        }
 
-      // TODO: Better check for if the response body is an user object or an auth error
-      if (_.has(result, 'code')) {
-        return next(result);
-      }
+        req.user = result;
 
-      req.user = result;
-
-      next();
-    });
+        next();
+      });
+    }
   };
 
   return {
--- src/sdk.js
+++ src/sdk.js
@@ -1,5 +1,6 @@
 var _ = require('underscore'),
   Amqp = require('amqp'),
+  Redis = require('redis'),
   config = require('./config'),
   errors = require('./errors'),
   middleware = require('./middleware'),
@@ -13,6 +14,7 @@ var _ = require('underscore'),
  * @param {object} options - Options stored and used by subcomponents.
  * @param {object} options.environment - The environment loaded.
  * @param {object} options.rabbitmq_connection_string - RabbitMQ conection string.
+ * @param {object} options.redis_connection_string - Redis conection string.
  * @static
  * @module contxt
  * @submodule sdk
@@ -21,13 +23,15 @@ var _ = require('underscore'),
  * @example
  *  var sdk = require('contxt-sdk-nodejs').Sdk({
  *    environment: 'development',
- *    rabbitmq_connection_string: 'amqp://0.0.0.0'
+ *    rabbitmq_connection_string: 'amqp://0.0.0.0',
+ *    redis_connection_string: 'redis://0.0.0.0:6379/0'
  *  });
  */
 
 var Sdk = function(options) {
   /**
    * @property _options
+   * @private
    * @type object
    */
   var _options = _.defaults(options, {
@@ -41,9 +45,9 @@ var Sdk = function(options) {
 
     rabbitmq_connection.ready = false;
 
-    rabbitmq_connection.on('error', function(error) {
+    rabbitmq_connection.on('error', function(err) {
       console.error('RABBITMQ ERROR');
-      console.log(error);
+      console.log(err);
     });
 
     rabbitmq_connection.on('ready', function() {
@@ -51,6 +55,23 @@ var Sdk = function(options) {
     });
 
     _options.rabbitmq_connection = rabbitmq_connection;
+  }
+
+  if (_.has(_options, 'redis_connection_string')) {
+    var redis_connection = Redis.createClient({
+      url: _options.redis_connection_string
+    });
+
+    redis_connection.on('error', function(err) {
+      console.error('REDIS ERROR');
+      console.log(err);
+    });
+
+    redis_connection.on('ready', function() {
+      redis_connection.ready = true;
+    });
+
+    _options.redis_connection = redis_connection;
   }
 
   config.set(_options);
