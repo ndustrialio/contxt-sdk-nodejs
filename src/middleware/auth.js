43b6aae345b7177158d063a52699f8fa75e12171
Ionut Moraru <morion4000@gmail.com>
2016-09-08 17:28:51 +0300

    Merged sdk.middleware.Auth.tokenCheck and sdk.middleware.Auth.jwtVerify into a single method
    * Removed legacy code
    * Removed unused imports
    * Updated documentation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,16 +1,14 @@
-var request = require('request'),
-  _ = require('underscore'),
+var _ = require('underscore'),
   jwt = require('express-jwt'),
   rsaValidation = require('auth0-api-jwt-rsa-validation'),
-  config = require('./../config'),
   errors = require('./../errors');
 
 /**
  * @class Auth
  * @constructor
  * @param {object} options - The options.
- * @param {object} options.service_url - The OAuth2 service URL.
- * @param {string} options.TOKEN_CACHE_EXPIRES - The expiration time of the auth token in seconds.
+ * @param {string} options.auth0_issuer - The Auth0 issuer URL.
+ * @param {string} options.auth0_audience - The Auth0 audience.
  * @static
  * @module middleware
  * @submodule auth
@@ -18,8 +16,8 @@ var request = require('request'),
  * @namespace contxt-sdk-nodejs.sdk.middleware
  * @example
  *  var auth = require('contxt-sdk-nodejs').Sdk().middleware.Auth({
- *    auth0_issuer_url: 'https://ndustrial.auth0.com/'
- *    auth0_audience: '<my_client_id>
+ *    auth0_issuer: 'https://ndustrial.auth0.com/'
+ *    auth0_audience: '<my_client_id>'
  *  });
  */
 
@@ -30,51 +28,44 @@ var Auth = function(options) {
    * @type object
    */
   var _options = _.defaults(options, {
-    TOKEN_CACHE_EXPIRES: 3600 // 1h
-  });
-  
-  if (!_.has(_options, 'client_id')) {
-    throw new Error('client_id must be provided!');
-  }
-
-  /**
-   * @property _config
-   * @private
-   * @type object
-   */
-  var _config = config.get();
-
-  
-  
-  var tokenCheck = function(req, res, next) {
-      if (!_.has(req.headers, 'authorization')) {
-          return next(new errors.validation_error('Authorization header not found!'));
-      }
-      var token = req.headers.authorization.split(' ');
-      if (token.length != 2) {
-          return next(new errors.validation_error('Invalid authorization header. Must be Bearer <token>'));
-      }
-      token = token[1];
-      var regex = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+\/=]*$/;
-      var result = token.match(regex);
-      if (!result) {
-          return next(new errors.validation_error('Invalid token. Must be JWT token.'));
-      }
-      
-      next();
-  }
-  
-  var jwtVerify = jwt({
-    secret: rsaValidation(),
-    algorithms: ['RS256'],
-    issuer: "https://ndustrial.auth0.com/",
-    audience: _options.client_id
+    auth0_issuer: 'https://ndustrial.auth0.com/'
   });
 
+  if (!_.has(_options, 'auth0_audience')) {
+    throw new Error('auth0_audience must be provided!');
+  }
+
+  var authorize = function(req, res, next) {
+    var regex = /^[A-Za-z0-9-_=]+\.[A-Za-z0-9-_=]+\.?[A-Za-z0-9-_.+\/=]*$/;
+
+    if (!_.has(req.headers, 'authorization')) {
+      return next(new errors.validation_error('Authorization header not found!'));
+    }
+
+    var token = req.headers.authorization.split(' ');
+
+    if (token.length !== 2) {
+      return next(new errors.validation_error('Invalid authorization header. Must be Bearer <token>'));
+    }
+
+    token = token[1];
+
+    var result = token.match(regex);
+
+    if (!result) {
+      return next(new errors.validation_error('Invalid token. Must be JWT token.'));
+    }
+
+    jwt({
+      secret: rsaValidation(),
+      algorithms: ['RS256'],
+      issuer: _options.auth0_issuer,
+      audience: _options.auth0_audience
+    })(req, res, next);
+  };
 
   return {
-    tokenCheck: tokenCheck,
-    jwtVerify: jwtVerify
+    authorize: authorize
   };
 };
 
