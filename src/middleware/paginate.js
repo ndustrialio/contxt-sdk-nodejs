e8d14521e724c351a8912479db513f5abacd6f05
Ionut Moraru <morion4000@gmail.com>
2016-09-09 17:15:19 +0300

    Updated paginate middleware documentation

--- src/middleware/paginate.js
+++ src/middleware/paginate.js
@@ -1,78 +1,76 @@
-var errors = require('./../errors'),
-    _ = require('underscore'),
-    config = require('./../config');
-
+var _ = require('underscore'),
+  errors = require('./../errors');
 
 /**
  * @class Paginate
  * @constructor
  * @param {object} options - The options.
- * @param {object} options.service_url - The OAuth2 service URL.
- * @param {string} options.TOKEN_CACHE_EXPIRES - The expiration time of the auth token in seconds.
  * @static
  * @module middleware
- * @submodule auth
+ * @submodule paginate
  * @main middleware
  * @namespace contxt-sdk-nodejs.sdk.middleware
  * @example
- *  var auth = require('contxt-sdk-nodejs').Sdk().middleware.Auth({
- *    service_url: 'test'
- *    TOKEN_CACHE_EXPIRES: 3600
- *  });
+ *  var paginate = require('contxt-sdk-nodejs').Sdk().middleware.Paginate({});
  */
+ 
 var Paginate = function(options) {
   /**
    * @property _options
    * @private
    * @type object
    */
-  var _options = _.defaults(options, {
-  });
+  var _options = _.defaults(options, {});
 
   /**
-   * @property _config
-   * @private
-   * @type object
-   */
-  var _config = config.get();
-  
-  /**
-   * 
+   * Handle pagination based on req values.
    *
+   * @member paginate
+   * @async
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {function} next - Callback to execute the next Express middleware.
    */
-  var paginate =  function (req, res, next) {
+  var paginate = function(req, res, next) {
     req.pagination = {};
 
     if (_.has(req.filters, 'offset')) {
-        req.pagination.offset = req.filters.offset;
-        delete req.filters.offset;
+      req.pagination.offset = req.filters.offset;
+
+      delete req.filters.offset;
     } else {
-        req.pagination.offset = 0;
+      req.pagination.offset = 0;
     }
+
     if (_.has(req.filters, 'limit')) {
-        req.pagination.limit = parseInt(req.filters.limit);
-        if (!req.pagination.limit) {
-            return next(new errors.validation_error('limit must be a number between 0 and 100'));
-        }
-        if (req.pagination.limit > 100) {
-            return next(new errors.validation_error('limit cannot be greater than 100'));
-        }
-        if (req.pagination.limit <= 0) {
-            return next(new errors.validation_error('limit cannot be 0 or less'));
-        }
-        delete req.filters.limit;
+      req.pagination.limit = parseInt(req.filters.limit);
+
+      if (!req.pagination.limit) {
+        return next(new errors.validation_error('limit must be a number between 0 and 100'));
+      }
+
+      if (req.pagination.limit > 100) {
+        return next(new errors.validation_error('limit cannot be greater than 100'));
+      }
+
+      if (req.pagination.limit <= 0) {
+        return next(new errors.validation_error('limit cannot be 0 or less'));
+      }
+
+      delete req.filters.limit;
     } else {
-        req.pagination.limit = 10;
+      req.pagination.limit = 10;
     }
 
     if (_.has(req.filters, 'orderBy')) {
-        req.pagination.orderBy = req.filters.orderBy;
-        delete req.filters.orderBy;
+      req.pagination.orderBy = req.filters.orderBy;
+
+      delete req.filters.orderBy;
     }
 
     next();
   };
-    
+
   return {
     paginate: paginate
   };
