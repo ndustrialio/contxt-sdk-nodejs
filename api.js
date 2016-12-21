2c02ecbe88a6e2f5c934f00fefaa0f71c977e47b
werckerbot <pleasemailus@wercker.com>
2016-12-21 10:00:40 +0000

    deploy from ionut-moraru

---
+++ api.js
@@ -0,0 +1,113 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "contxt-sdk-nodejs.Config",
+        "contxt-sdk-nodejs.Errors",
+        "contxt-sdk-nodejs.Request",
+        "contxt-sdk-nodejs.RoutesScanner",
+        "contxt-sdk-nodejs.Sdk",
+        "contxt-sdk-nodejs.Util",
+        "contxt-sdk-nodejs.sdk.Listener",
+        "contxt-sdk-nodejs.sdk.middleware.Auth",
+        "contxt-sdk-nodejs.sdk.middleware.Auth0",
+        "contxt-sdk-nodejs.sdk.middleware.Cors",
+        "contxt-sdk-nodejs.sdk.middleware.Handlers",
+        "contxt-sdk-nodejs.sdk.middleware.Legacy",
+        "contxt-sdk-nodejs.sdk.middleware.Notifier",
+        "contxt-sdk-nodejs.sdk.middleware.Paginate",
+        "contxt-sdk-nodejs.sdk.middleware.Permissions",
+        "contxt-sdk-nodejs.sdk.middleware.Routers"
+    ],
+    "modules": [
+        "auth",
+        "auth0",
+        "config",
+        "contxt",
+        "cors",
+        "handlers",
+        "legacy",
+        "middleware",
+        "notifier",
+        "paginate",
+        "permissions",
+        "request",
+        "routes",
+        "routescanner",
+        "sdk",
+        "util"
+    ],
+    "allModules": [
+        {
+            "displayName": "auth",
+            "name": "auth"
+        },
+        {
+            "displayName": "auth0",
+            "name": "auth0"
+        },
+        {
+            "displayName": "config",
+            "name": "config",
+            "description": "Store and retrive configurations."
+        },
+        {
+            "displayName": "contxt",
+            "name": "contxt",
+            "description": "Store and retrive configurations."
+        },
+        {
+            "displayName": "cors",
+            "name": "cors"
+        },
+        {
+            "displayName": "handlers",
+            "name": "handlers"
+        },
+        {
+            "displayName": "legacy",
+            "name": "legacy"
+        },
+        {
+            "displayName": "middleware",
+            "name": "middleware"
+        },
+        {
+            "displayName": "notifier",
+            "name": "notifier"
+        },
+        {
+            "displayName": "paginate",
+            "name": "paginate"
+        },
+        {
+            "displayName": "permissions",
+            "name": "permissions"
+        },
+        {
+            "displayName": "request",
+            "name": "request",
+            "description": "Allows to call remote services."
+        },
+        {
+            "displayName": "routes",
+            "name": "routes"
+        },
+        {
+            "displayName": "routescanner",
+            "name": "routescanner",
+            "description": "Generates scope definitions based on route files."
+        },
+        {
+            "displayName": "sdk",
+            "name": "sdk",
+            "description": "Listen for messages on queue."
+        },
+        {
+            "displayName": "util",
+            "name": "util",
+            "description": "Provides useful functions."
+        }
+    ],
+    "elements": []
+} };
+});
